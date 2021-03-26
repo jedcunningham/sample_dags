1ca7878a6824e3a0c4083633f1e2305128c1c832
Jed Cunningham <66968678+jedcunningham@users.noreply.github.com>
2021-03-26 16:12:21 -0600

    Initial

---
+++ sleep.py
@@ -0,0 +1,18 @@
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+from airflow.utils.dates import days_ago
+
+args = {
+    "owner": "airflow",
+}
+
+with DAG(
+    dag_id="sleep",
+    default_args=args,
+    schedule_interval=None,
+    start_date=days_ago(2),
+) as dag:
+    BashOperator(
+        task_id="sleep",
+        bash_command="date; sleep 30; date",
+    )
