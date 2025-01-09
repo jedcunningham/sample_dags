f4bf9fbd0a53cf0c79df3929ecea17fa1e740000
Jed Cunningham <66968678+jedcunningham@users.noreply.github.com>
2025-01-08 19:43:42 -0700

    Add demo file

---
+++ demo.py
@@ -0,0 +1,9 @@
+from airflow import DAG
+from airflow.providers.standard.operators.bash import BashOperator
+
+with DAG(dag_id="demo", schedule=None):
+    sleep = BashOperator(task_id="sleep", bash_command="sleep 45")
+    hello = BashOperator(task_id="hello", bash_command="Hello version 1")
+
+    sleep >> hello
+
