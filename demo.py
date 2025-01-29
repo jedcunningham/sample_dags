6be707fce789cb9a65b505c474d610862327d85d
Jed Cunningham <66968678+jedcunningham@users.noreply.github.com>
2025-01-28 21:32:13 -0700

    updating

--- demo.py
+++ demo.py
@@ -3,7 +3,7 @@ from airflow.providers.standard.operators.bash import BashOperator
 
 with DAG(dag_id="demo"):
     sleep = BashOperator(task_id="sleep", bash_command="sleep 60")
-    hello = BashOperator(task_id="hello", bash_command="echo 'Hello version 3'")
+    hello = BashOperator(task_id="hello", bash_command="echo 'Hello version 4'")
 
     sleep >> hello
 
