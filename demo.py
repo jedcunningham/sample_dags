2206179bcdc98a2819632eb5a9a92ad2a5a9226f
Jed Cunningham <66968678+jedcunningham@users.noreply.github.com>
2025-04-01 08:45:33 -0600

    up

--- demo.py
+++ demo.py
@@ -2,16 +2,9 @@ from airflow import DAG
 from airflow.providers.standard.operators.bash import BashOperator
 
 with DAG(dag_id="demo"):
-    # First
-    sleep = BashOperator(task_id="sleep", bash_command="sleep 5")
-    hello = BashOperator(task_id="hello", bash_command="echo 'Hello'")
-    astronomer = BashOperator(task_id="astronomer", bash_command="echo 'Astonomer2!!'")
-
-    sleep >> hello >> astronomer
-
     # Second run
-    #sleep = BashOperator(task_id="sleep", bash_command="sleep 1")
-    #hello = BashOperator(task_id="hello", bash_command="echo 'Hello Astronomer!!'")
+    sleep = BashOperator(task_id="sleep", bash_command="sleep 1")
+    hello = BashOperator(task_id="hello", bash_command="echo 'Hello Astronomer!!'")
 
-    #sleep >> hello
+    sleep >> hello
 
