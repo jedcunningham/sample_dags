4865d4a1539913e47c1b798514e718573ae92e08
Jed Cunningham <66968678+jedcunningham@users.noreply.github.com>
2025-06-02 11:03:47 -0600

    ...

--- syspath.py
+++ syspath.py
@@ -3,6 +3,8 @@ import pprint
 
 from airflow.sdk import DAG, task
 
+from foo import HI
+
 pprint.pprint(sys.path)
 
 with DAG(dag_id="syspath"):
@@ -10,6 +12,8 @@ with DAG(dag_id="syspath"):
     @task
     def print_syspath():
         pprint.pprint(sys.path)
+
+    print_syspath()
 
 
 
