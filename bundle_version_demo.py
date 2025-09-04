68257c07373708e6e24889b2e9fbefb4e8ae7ca1
Jed Cunningham <66968678+jedcunningham@users.noreply.github.com>
2025-09-04 12:17:33 -0600

    Add new demo file

---
+++ bundle_version_demo.py
@@ -0,0 +1,8 @@
+from airflow.sdk import DAG
+from airflow.providers.standard.operators.bash import BashOperator
+
+with DAG(dag_id="bundle_version_demo"):
+    sleep = BashOperator(task_id="sleep", bash_command="sleep 45")
+    hello = BashOperator(task_id="hello", bash_command="echo 'Hello Everyone!'")
+    sleep >> hello
+
