08dd39a87eccf85d54c865bb97383d27f5fe677a
Jed Cunningham <66968678+jedcunningham@users.noreply.github.com>
2025-09-04 12:45:50 -0600

    up

--- bundle_version_demo.py
+++ bundle_version_demo.py
@@ -3,6 +3,6 @@ from airflow.providers.standard.operators.bash import BashOperator
 
 with DAG(dag_id="bundle_version_demo"):
     sleep = BashOperator(task_id="sleep", bash_command="sleep 60")
-    hello = BashOperator(task_id="hello_v1", bash_command="echo 'Hello everyone!'")
+    hello = BashOperator(task_id="hello_v2", bash_command="echo 'Hello world!'")
     sleep >> hello
 
