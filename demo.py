a01a9ac0f170d4a22258f541e4c6c4f9c8763f4d
Jed Cunningham <66968678+jedcunningham@users.noreply.github.com>
2025-09-25 11:09:19 -0600

    up

--- demo.py
+++ demo.py
@@ -1,6 +1,8 @@
 from airflow import DAG
 from airflow.providers.standard.operators.bash import BashOperator
 
+#hi
+
 with DAG(dag_id="demo"):
     # Second run
     sleep = BashOperator(task_id="sleep", bash_command="sleep 60")
