00621618fb939ab6fae8deccb84e794daae86a1f
Jed Cunningham <66968678+jedcunningham@users.noreply.github.com>
2025-09-25 07:43:20 -0600

    up

--- demo.py
+++ demo.py
@@ -4,7 +4,7 @@ from airflow.providers.standard.operators.bash import BashOperator
 with DAG(dag_id="demo"):
     # First run
     sleep = BashOperator(task_id="sleep", bash_command="sleep 60")
-    hello = BashOperator(task_id="hello2", bash_command="echo 'Hello Airflow Summit!'")
+    hello = BashOperator(task_id="hello", bash_command="echo 'Hello Airflow Summit!'")
 
     sleep >> hello
 
